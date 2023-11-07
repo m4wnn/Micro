baf14c6beca9dd4a35c2edcab98decc2df86351d
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2023-11-07 00:17:31 -0800

    Create join_answers.sh

---
+++ join_answers.sh
@@ -0,0 +1,20 @@
+# Verifies that the answers file exists and is empty.
+
+if [ -f answers.md ]; then
+    rm answers.md
+fi
+
+# Create the answers file.
+touch answers.md
+
+# Add the answers to the answers file.
+for file in *.md
+do
+    echo "## $file" >> answers.md
+    echo "" >> answers.md
+    cat $file >> answers.md
+    echo "" >> answers.md
+done
+
+# Success message.
+echo "answers.md created successfully at $(date +"%T") on $(date +"%D")"
